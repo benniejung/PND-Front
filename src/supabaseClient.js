f0994c6360785ff0d364f04352c8100541bbdf71
binniejung <ybe1027@naver.com>
2025-11-24 20:49:30 +0900

    📝  docs: README 작성

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,18 @@
 import { createClient } from "@supabase/supabase-js";
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+
+if (!supabaseUrl) {
+  throw new Error(
+    "Missing env.VITE_SUPABASE_URL, see .env.example for required variables"
+  );
+}
+
+if (!supabasePublishableKey) {
+  throw new Error(
+    "Missing env.VITE_SUPABASE_PUBLISHABLE_KEY, see .env.example for required variables"
+  );
+}
+
 export const supabase = createClient(supabaseUrl, supabasePublishableKey);
